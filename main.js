d15138dc49f16e33e9396c6106b060e7b046ecb9
SexyBoy3 <116267189+SexyBoy3@users.noreply.github.com>
2023-05-29 14:01:51 +0300

    Added functions to server

--- main.js
+++ main.js
@@ -49,3 +49,110 @@ var initHttpServer = () => {
     app.listen(http_port, () => console.log('Listening http on port: ' + 
     http_port));
     };
+var initP2PServer = () => {
+        var server = new WebSocket.Server({port: p2p_port});
+        server.on('connection', ws => initConnection(ws));
+        console.log('listening websocket p2p port on: ' + p2p_port);
+        };
+var initConnection = (ws) => {
+        sockets.push(ws);
+        initMessageHandler(ws);
+        initErrorHandler(ws);
+        write(ws, queryChainLengthMsg());
+            };
+
+    var initMessageHandler = (ws) => {
+    ws.on('message', (data) => {
+    var message = JSON.parse(data);
+    console.log('Received message' + JSON.stringify(message));
+    switch (message.type) {
+    case MessageType.QUERY_LATEST:
+    write(ws, responseLatestMsg());
+    break;
+    case MessageType.QUERY_ALL:
+    write(ws, responseChainMsg());
+    break;
+    case MessageType.RESPONSE_BLOCKCHAIN:
+    handleBlockchainResponse(message);
+    break;
+    }
+    });
+    };
+    var initErrorHandler = (ws) => {
+var closeConnection = (ws) => {
+    console.log('connection failed to peer: ' + ws.url);
+    sockets.splice(sockets.indexOf(ws), 1);
+        };
+    ws.on('close', () => closeConnection(ws));
+    ws.on('error', () => closeConnection(ws));
+        };  
+        var connectToPeers = (newPeers) => {
+            newPeers.forEach((peer) => {
+            var ws = new WebSocket(peer);
+            ws.on('open', () => initConnection(ws));
+            ws.on('error', () => {
+            console.log('connection failed')
+            });
+        });
+    };
+    var handleBlockchainResponse = (message) => {
+        var receivedBlocks = JSON.parse(message.data).sort((b1, b2) => (b1.index -
+        b2.index));
+        var latestBlockReceived = receivedBlocks[receivedBlocks.length - 1];
+        var latestBlockHeld = getLatestBlock();
+        if (latestBlockReceived.index > latestBlockHeld.index) {
+        console.log('blockchain possibly behind. We got: ' + 
+        latestBlockHeld.index + ' Peer got: ' + latestBlockReceived.index);
+        if (latestBlockHeld.hash === latestBlockReceived.previousHash) {
+        console.log("We can append the received block to our chain");
+        blockchain.push(latestBlockReceived);
+        broadcast(responseLatestMsg());
+        } else if (receivedBlocks.length === 1) {
+        console.log("We have to query the chain from our peer");
+        broadcast(queryAllMsg());
+        } else {
+        console.log("Received blockchain is longer than current blockchain");
+        replaceChain(receivedBlocks);
+        }
+        } else {
+        console.log('received blockchain is not longer than current blockchain. Do nothing');
+        }
+        };
+        var generateNextBlock = (blockData) => {
+            var previousBlock = getLatestBlock();
+            var nextIndex = previousBlock.index + 1;
+            var nextTimestamp = new Date().getTime() / 1000;
+            var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, 
+            blockData);
+            return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, 
+                nextHash);
+                };
+                var calculateHashForBlock = (block) => {
+                return calculateHash(block.index, block.previousHash, block.timestamp, 
+                block.data);
+                };
+                var calculateHash = (index, previousHash, timestamp, data) => {
+                return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+                };
+                var addBlock = (newBlock) => {
+                if (isValidNewBlock(newBlock, getLatestBlock())) {
+                blockchain.push(newBlock);
+                }
+                };
+                var isValidNewBlock = (newBlock, previousBlock) => {
+                if (previousBlock.index + 1 !== newBlock.index) {
+                console.log('invalid index');
+                return false;
+                } else if (previousBlock.hash !== newBlock.previousHash) {
+                console.log('invalid previoushash');
+                return false;
+                } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+                console.log(typeof (newBlock.hash) + ' ' + typeof
+                calculateHashForBlock(newBlock));
+                console.log('invalid hash: ' + calculateHashForBlock(newBlock) + ' ' + 
+                newBlock.hash);
+                return false;
+                }
+                return true;
+                };
+                
